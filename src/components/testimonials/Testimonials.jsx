7003be86d4ded4b11a51504d0adf1e3b4dddb99c
FREEDOM <freewhynane62@gmail.com>
2023-02-02 03:49:27 +0000

    add testimonials component with logic

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./testimonials.css";
+import Data from "./Data";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const Testimonials = () => {
+  return (
+    <section className="testimonials container section" id="testimonials">
+      <h2 className="section__title">Client & Reviews</h2>
+
+      <Swiper
+        className="testimonials__container grid"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+      >
+        {Data.map(({ id, image, title, subtitle, comment }) => {
+          return (
+            <SwiperSlide className="testimonials__item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+              <h3 className="testimonials__title">{title}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
