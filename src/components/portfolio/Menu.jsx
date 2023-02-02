d464fc8d47bf80287f528dcc776b8d524b10cbb4
FREEDOM <freewhynane62@gmail.com>
2023-02-02 03:42:39 +0000

    add menu as array for portfolio

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable no-sparse-arrays */
+import Work1 from "../../assets/work-6.png";
+import Work2 from "../../assets/work-2.png";
+import Work3 from "../../assets/work-3.png";
+import Work4 from "../../assets/work-4.png";
+import Work5 from "../../assets/work-5.png";
+import Work6 from "../../assets/work-1.png";
+import Work7 from "../../assets/work-7.png";
+import Work8 from "../../assets/work-8.png";
+import Work9 from "../../assets/work-9.png";
+
+const Menu = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Project Management Illustration",
+    category: "Design",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Guest App Walkthrough Screens",
+    category: "Mobile",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Delivery App Wireframe",
+    category: "Web",
+  },
+  ,
+  {
+    id: 4,
+    image: Work4,
+    title: "Onboarding Motivation",
+    category: "Design",
+  },
+  ,
+  {
+    id: 5,
+    image: Work5,
+    title: "iMac Mockup Design",
+    category: "Web",
+  },
+
+  ,
+  {
+    id: 6,
+    image: Work6,
+    title: "Game Store App Concept",
+    category: "Mobile",
+  },
+  ,
+  {
+    id: 7,
+    image: Work7,
+    title: "iMac Mockup Design",
+    category: "Web",
+  },
+  ,
+  {
+    id: 8,
+    image: Work8,
+    title: "iMac Mockup Design",
+    category: "Web",
+  },
+  ,
+  {
+    id: 9,
+    image: Work9,
+    title: "iMac Mockup Design",
+    category: "Web",
+  },
+];
+
+export default Menu;
