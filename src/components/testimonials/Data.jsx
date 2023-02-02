9615e94024009c3fe8793a40fda3af6844643a2d
FREEDOM <freewhynane62@gmail.com>
2023-02-02 03:47:27 +0000

    add data as array for testimonials

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,31 @@
+import Image1 from "../../assets/avatar-1.svg";
+import Image3 from "../../assets/avatar-3.svg";
+
+const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Emmanuel",
+    subtitle: "Team mate",
+    comment:
+      "I was impressed by Freedom's work ethic, his ability to meet deadlines and willingness to help his team members. He is a great communicator and a valuable member of the team.",
+  },
+  {
+    id: 2,
+    image: Image3,
+    title: "Isacc",
+    subtitle: "Team mate",
+    comment:
+      "I had the pleasure of working with Freedom and I can confidently say that He is  an asset to any development team. His technical skills are top-notch and his ability to collaborate and problem-solve is exceptional.",
+  },
+  {
+    id: 2,
+    image: Image3,
+    title: "Mr. Francis",
+    subtitle: "Client",
+    comment:
+      "Freedom's ability to deliver projects on time and within budget is impressive. His communication skills are excellent and he is always willing to go the extra mile to ensure client satisfaction.",
+  },
+];
+
+export default Data;
