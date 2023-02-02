e2329b64566599540e46bc6d64eafc214904a61a
FREEDOM <freewhynane62@gmail.com>
2023-02-02 03:39:29 +0000

    add Post as blog array

---
+++ src/components/blog/Post.jsx
@@ -0,0 +1,31 @@
+const Post = [
+  {
+    id: 1,
+    link: "education",
+    image:
+      "https://png.pngtree.com/png-vector/20220804/ourlarge/pngtree-programmers-using-javascript-programming-language-on-computer-png-image_5737687.png",
+    date: "18 January, 2023",
+    title: "Unleashing the Power of Javascript in Web Development",
+  },
+  {
+    id: 3,
+    link: "education",
+    image:
+      "https://img.freepik.com/free-vector/ui-ux-differences-concept-illustration_114360-11070.jpg?w=740&t=st=1673858582~exp=1673859182~hmac=df28817d1822299d11fed16e68b5f95d38d8c5ac30f4b4a0947ccd33c6927d4c",
+    date: "12 january, 2023",
+    title:
+      "Creating a Seamless User Experience: Best Practices for UI/UX Design",
+  },
+
+  {
+    id: 2,
+    link: "education",
+    image:
+      "https://www.insiderintelligence.com/static/5ecb856d09def78da57ec3fac2f6ce5c/72c11/blockchain.webp",
+    date: "27 December, 2022",
+    title:
+      "Building a Better Future with Blockchain: Transforming Industries and Transactions",
+  },
+];
+
+export default Post;
