0c5c9088a6378fc77d5778b51afad7106abe9cf0
FREEDOM <freewhynane62@gmail.com>
2023-02-02 03:37:20 +0000

    makes component available for import

---
+++ src/components/index.jsx
@@ -0,0 +1,23 @@
+import Sidebar from "./sidebar/Sidebar";
+import Home from "./home/Home";
+import Portfolio from "./portfolio/Portfolio";
+import Resume from "./resume/Resume";
+import Services from "./services/Services";
+import Pricing from "./pricing/Pricing";
+import Testimonials from "./testimonials/Testimonials";
+import Blog from "./blog/Blog";
+import Contact from "./contact/Contact";
+import About from "./about/About";
+
+export {
+  Sidebar,
+  Home,
+  Portfolio,
+  Resume,
+  Services,
+  Pricing,
+  Testimonials,
+  Blog,
+  Contact,
+  About,
+};
