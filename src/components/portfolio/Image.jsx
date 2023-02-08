ba4c73b2a471b86eb2c5e95e6ec04080d66a994d
FREEDOM <freewhynane62@gmail.com>
2023-02-08 05:03:30 +0000

    add image component

---
+++ src/components/portfolio/Image.jsx
@@ -0,0 +1,63 @@
+import Work1 from "../../assets/work-6.png";
+import Work2 from "../../assets/work-2.png";
+import Work3 from "../../assets/work-3.png";
+import Work4 from "../../assets/work-4.png";
+import Work5 from "../../assets/work-5.png";
+import Work6 from "../../assets/work-1.png";
+import Work7 from "../../assets/work-7.png";
+import Work8 from "../../assets/work-8.png";
+import Work9 from "../../assets/work-9.png";
+import Work10 from "../../assets/work-11.png";
+import Work11 from "../../assets/work-12.png";
+import Work12 from "../../assets/work-13.png";
+import Work13 from "../../assets/work-14.png";
+import Work14 from "../../assets/work-15.png";
+import Work15 from "../../assets/work-16.png";
+import Work16 from "../../assets/work-17.png";
+import Work17 from "../../assets/work-18.png";
+import Work18 from "../../assets/work-19.png";
+import Work19 from "../../assets/work-20.png";
+import Work20 from "../../assets/work-21.png";
+import Work21 from "../../assets/work-22.png";
+import Work22 from "../../assets/work-23.png";
+import Work23 from "../../assets/work-24.png";
+import Work24 from "../../assets/work-25.png";
+import Work25 from "../../assets/work-26.png";
+import Work26 from "../../assets/work-27.png";
+// import Work27 from "../../assets/work-28.png";
+// import Work28 from "../../assets/work-29.png";
+// import Work29 from "../../assets/work-30.png";
+// import Work30 from "../../assets/work-31.png";
+
+export {
+  Work1,
+  Work2,
+  Work3,
+  Work4,
+  Work5,
+  Work6,
+  Work7,
+  Work8,
+  Work9,
+  Work10,
+  Work11,
+  Work12,
+  Work13,
+  Work14,
+  Work15,
+  Work16,
+  Work17,
+  Work18,
+  Work19,
+  Work20,
+  Work21,
+  Work22,
+  Work23,
+  Work24,
+  Work25,
+  Work26,
+  //   Work27,
+  //   Work28,
+  //   Work29,
+  //   Work30,
+};
