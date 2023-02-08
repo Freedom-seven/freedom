4405e794609795d6d78d44239f4db9bea7d91376
FREEDOM <freewhynane62@gmail.com>
2023-02-08 05:03:17 +0000

    add avatar image with styles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-import Me from "../../assets/avatar-1.svg";
+import Avatar1 from "../../assets/avatar-10.png";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 import Shapes from "./Shapes";
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
-        <img src={Me} alt="" className="home__img" />
+        <img src={Avatar1} alt="" className="home__img" />
         <h1 className="home__name">Nanewortor Patrick Freedom</h1>
         <span className="home__education">I'm a Software developer</span>
 
