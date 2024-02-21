962bfa87c6eb9d5926b14eeffc5551844cc51c4b
FREEDOM <freewhynane62@gmail.com>
2024-02-21 03:34:56 +0000

    modify

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -17,6 +17,10 @@ const HeaderSocials = () => {
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: [1, 1.2, 1, 1.2, 1] }}
         transition={{ delay: 0.5, duration: 2 }}
+        whileHover={{
+          scale: 1.3,
+          transition: { type: "spring", stiffness: 300 },
+        }}
       >
         <i className="fa-brands fa-linkedin-in"></i>
       </motion.a>
@@ -29,8 +33,27 @@ const HeaderSocials = () => {
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: [1, 1.2, 1, 1.2, 1] }}
         transition={{ delay: 1, duration: 2 }}
+        whileHover={{
+          scale: 1.3,
+          transition: { type: "spring", stiffness: 300 },
+        }}
       >
         <i className="fa-brands fa-twitter"></i>
+      </motion.a>
+      <motion.a
+        href="https://www.upwork.com/freelancers/~012c1f8db03f75c35f"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: [1, 1.2, 1, 1.2, 1] }}
+        whileHover={{
+          scale: 1.3,
+          transition: { type: "spring", stiffness: 300 },
+        }}
+        transition={{ delay: 1, duration: 2 }}
+      >
+        <i className="fa-brands fa-upwork"></i>
       </motion.a>
 
       <motion.a
@@ -41,6 +64,10 @@ const HeaderSocials = () => {
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: [1, 1.2, 1, 1.2, 1] }}
         transition={{ delay: 1.5, duration: 2 }}
+        whileHover={{
+          scale: 1.3,
+          transition: { type: "spring", stiffness: 300 },
+        }}
       >
         <i className="fa-brands fa-github"></i>
       </motion.a>
